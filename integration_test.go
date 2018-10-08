d25d4de92f6f9e839ab8c3a5ebeab24ecc1e9982
Ashish Aggarwal <asaggarwal@DELC02T84M5G8WN.sea.corp.expecn.com>
2018-10-08 14:29:23 +0530

    Add few more asserts in test code

--- integration_test.go
+++ integration_test.go
@@ -18,7 +18,9 @@
 package haystack
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 	"testing"
 	"time"
 
@@ -37,14 +39,16 @@ func TestIntegrationWithHaystackAgent(t *testing.T) {
 		}
 	}()
 
-	serverSpan := tracer.StartSpan("serverOp", opentracing.Tag{Key: "my-tag", Value: "something"})
+	serverTime := time.Now()
+	serverSpan := tracer.StartSpan("serverOp", opentracing.Tag{Key: "server-tag-key", Value: "something"}, opentracing.StartTime(serverTime))
 	ext.SpanKind.Set(serverSpan, ext.SpanKindRPCServerEnum)
 	ext.Error.Set(serverSpan, false)
 	ext.HTTPStatusCode.Set(serverSpan, 200)
 	ext.HTTPMethod.Set(serverSpan, "POST")
 	serverSpan.LogEventWithPayload("code", 1001)
 
-	clientSpan := tracer.StartSpan("clientOp", opentracing.ChildOf(serverSpan.Context()))
+	clientTime := time.Now()
+	clientSpan := tracer.StartSpan("clientOp", opentracing.ChildOf(serverSpan.Context()), opentracing.StartTime(clientTime))
 	ext.SpanKind.Set(clientSpan, ext.SpanKindRPCClientEnum)
 	ext.Error.Set(clientSpan, true)
 	ext.HTTPStatusCode.Set(clientSpan, 404)
@@ -78,12 +82,16 @@ func TestIntegrationWithHaystackAgent(t *testing.T) {
 
 	clientSpanReceived := 0
 	serverSpanReceived := 0
+	clientParentSpanID := ""
+	clientTraceID := ""
+
+	serverSpanID := ""
+	serverTraceID := ""
 
 ConsumerLoop:
 	for {
 		select {
 		case msg := <-partitionConsumer.Messages():
-			log.Printf("Consumed message offset %d\n", msg.Offset)
 			span := &Span{}
 			unmarshalErr := span.XXX_Unmarshal(msg.Value)
 			if unmarshalErr != nil {
@@ -91,24 +99,37 @@ ConsumerLoop:
 			}
 
 			verifyCommonAttr(t, span)
-
 			for _, tag := range span.GetTags() {
 				if tag.GetKey() == "span.kind" {
 					switch tag.GetVStr() {
 					case "client":
 						clientSpanReceived = clientSpanReceived + 1
 						assert.Equal(t, span.GetOperationName(), "clientOp")
+						assert.Equal(t, span.StartTime, int64(clientTime.UnixNano()/int64(time.Microsecond)))
+						assert.Equal(t, tagVal(span, "server-tag-key"), "")
+						assert.Equal(t, tagVal(span, "error"), "true")
+						assert.Equal(t, tagVal(span, string(ext.HTTPStatusCode)), "404")
+						clientParentSpanID = span.GetParentSpanId()
+						clientTraceID = span.GetTraceId()
 					case "server":
 						serverSpanReceived = serverSpanReceived + 1
 						assert.Equal(t, span.GetOperationName(), "serverOp")
+						assert.Equal(t, span.StartTime, int64(serverTime.UnixNano()/int64(time.Microsecond)))
+						assert.Equal(t, tagVal(span, "server-tag-key"), "something")
+						assert.Equal(t, tagVal(span, "error"), "false")
+						assert.Equal(t, tagVal(span, string(ext.HTTPStatusCode)), "200")
+						serverSpanID = span.GetSpanId()
+						serverTraceID = span.GetTraceId()
 					}
 				}
 			}
 
-			// expect only two spans
+			// expect only two spans and compare the ID relationship
 			if msg.Offset == 1 {
 				assert.Equal(t, clientSpanReceived, 1)
 				assert.Equal(t, serverSpanReceived, 1)
+				assert.Equal(t, serverSpanID, clientParentSpanID)
+				assert.Equal(t, serverTraceID, clientTraceID)
 				break ConsumerLoop
 			}
 		}
@@ -117,11 +138,23 @@ ConsumerLoop:
 
 func verifyCommonAttr(t *testing.T, span *Span) {
 	assert.Equal(t, span.GetServiceName(), "dummy-service")
-	appVersionTag := ""
+	assert.Equal(t, tagVal(span, "appVer"), "v1.1")
+}
+
+func tagVal(span *Span, tagKey string) string {
 	for _, tag := range span.GetTags() {
-		if tag.GetKey() == "appVer" {
-			appVersionTag = tag.GetVStr()
+		if tag.GetKey() == tagKey {
+			switch tag.GetType() {
+			case Tag_STRING:
+				return tag.GetVStr()
+			case Tag_BOOL:
+				return strconv.FormatBool(tag.GetVBool())
+			case Tag_LONG:
+				return fmt.Sprintf("%d", tag.GetVLong())
+			case Tag_DOUBLE:
+				return fmt.Sprintf("%f", tag.GetVDouble())
+			}
 		}
 	}
-	assert.Equal(t, appVersionTag, "v1.1")
+	return ""
 }
